0b79816204529268aacde931d5fa0c9ce5e70120
Sebastian <srosero87@misena.edu.co>
2024-09-06 13:54:05 +0200

    Fix: Ajustes de bugs

--- assets/app.js
+++ assets/app.js
@@ -2,136 +2,163 @@ const input = document.querySelector(".input input")
 const addTask = document.querySelector(".input button")
 const taskContainer = document.querySelector(".taskContainer")
 
+const randomId = () => {
+  return Math.floor(Math.random() * 1000)
+}
 
-
-let id = 0
 taskList = []
 addTask.addEventListener("click", ()=>{
 
-const nuevaTarea = input.value
-const regex = /^[a-zA-Z0-9\s]*$/
+  const nuevaTarea = input.value
+  const regex = /^[a-zA-Z0-9\s]*$/
 
-if(!regex.test(nuevaTarea) || nuevaTarea == ""){
+  if(!regex.test(nuevaTarea) || nuevaTarea == ""){
     alert("solo caracteres alfabéticos")
-   return
-}
+    return
+  }
 
+  const id = randomId();
 
-id++
-const newTask = document.createElement("div")
-newTask.innerHTML = `<p>${nuevaTarea}</p> <button class="detalles">Detalles</button>
-<button class="modificar">Modificar</button>
-<button class="eliminar">Eliminar</button>`
-newTask.classList.add("tareas")
-newTask.setAttribute("id", `${id}`)
-taskContainer.append(newTask)
+  const newTask = document.createElement("div");
 
-taskList.push(newTask.outerHTML)
-input.value = ""
+  newTask.innerHTML = 
+  `<p>${nuevaTarea}</p> <button class="detalles">Detalles</button>
+  <button class="modificar">Modificar</button>
+  <button data-id="${id}" class="eliminar">Eliminar</button>`
 
-const tareas = document.querySelectorAll(".tareas")
-tareas.forEach(()=>{
-  const modalDetalle = document.createElement("div")
-   modalDetalle.innerHTML =  
-  ` <h2>Modificar detalles</h2>
-   <label for="input">Tarea</label>
-   <input type="text" name="tarea">
-   <label for="input">Descripción</label>
-   <input type="text" name="desc">
-   <label for="input">Fecha y Hora entrega</label>
-   <input type="text" name="fecha_hora">
-   <button class="h-25 w-50" id="modificar">Guardar</button>
-`
-modalDetalle.classList.add("modalDetalle")
-modalDetalle.style.display = "none"
-taskContainer.append(modalDetalle)
+  newTask.classList.add("tareas")
+  newTask.setAttribute("id", `${id}`)
+  taskContainer.append(newTask)
 
-})
+  const taskDetails = {
+    id: id,
+    task: nuevaTarea,
+    description: "",
+    createdAt: new Date().toLocaleDateString(),
+  }
 
-const modificar = document.querySelectorAll(".tareas .modificar")
-const modales = document.querySelectorAll(".modalDetalle") 
+  taskList.push(taskDetails);
+  localStorage.setItem('tareasJavi', JSON.stringify(taskList));
 
-modificar.forEach((mod, j)=>{
-  mod.addEventListener("click",()=>{
-    modales[j].style.display = "flex"
+  input.value = ""
+
+  const tareas = document.querySelectorAll(".tareas")
+  tareas.forEach(()=>{
+      const modalDetalle = document.createElement("div")
+      modalDetalle.innerHTML =  
+      ` <h2>Modificar detalles</h2>
+      <label for="input">Tarea</label>
+      <input type="text" name="tarea">
+      <label for="input">Descripción</label>
+      <input type="text" name="desc">
+      <label for="input">Fecha y Hora entrega</label>
+      <input type="date" name="fecha_hora">
+      <button class="h-25 w-50" id="modificar">Guardar</button>
+    `
+    modalDetalle.classList.add("modalDetalle")
+    modalDetalle.style.display = "none"
+    taskContainer.append(modalDetalle)
+
   })
-})
 
-const guardar = document.querySelectorAll(".modalDetalle #modificar")
+  const modificar = document.querySelectorAll(".tareas .modificar")
+  const modales = document.querySelectorAll(".modalDetalle") 
 
-guardar.forEach((gu, k)=>{
-  gu.addEventListener("click",()=>{
-    modales[k].style.display = "none"
+  modificar.forEach((mod, j)=>{
+    mod.addEventListener("click",()=>{
+      modales[j].style.display = "flex"
+    })
   })
-})
 
-const taskRescue = document.querySelectorAll(".tareas .eliminar")
-const tareastoDel = taskContainer.querySelectorAll(".tareas")
+  const guardar = document.querySelectorAll(".modalDetalle #modificar")
 
-taskRescue.forEach((tr,i)=>{
-  tr.addEventListener("click",()=>{
-    tareastoDel[i].remove()
+  guardar.forEach((gu, k)=>{
+    gu.addEventListener("click",()=>{
+      modales[k].style.display = "none"
+    })
   })
+
+  const taskDelete = document.querySelectorAll(".tareas .eliminar")
+  const tareastoDel = taskContainer.querySelectorAll(".tareas")
+  
+  taskDelete.forEach((td,i)=>{
+    td.addEventListener("click",()=>{
+      const getId = td.getAttribute("data-id")
+
+      const tasksFiltered = taskList.filter( (task) => {
+        return task.id != getId
+      });
+
+      localStorage.setItem('tareasJavi', JSON.stringify(tasksFiltered));
+
+      tareastoDel[i].remove()
+    })
+  })
+  
 })
-
-localStorage.setItem('tareasJavi', JSON.stringify(taskList));
-localStorage.setItem('idTareas' , JSON.stringify(id))
-
-})
-
-
 
 document.addEventListener("DOMContentLoaded", () => {
  
   const getLocalStorage = localStorage.getItem('tareasJavi')
-     
   const tareasPendientes = JSON.parse(getLocalStorage)
  
- taskContainer.innerHTML = tareasPendientes
+  if( tareasPendientes ){
 
+    tareasPendientes.forEach(tarea => {
+      const nuevaTarea = document.createElement("div")
+      nuevaTarea.setAttribute("id", `${tarea.id}`)
+      nuevaTarea.innerHTML = 
+      `<p>${tarea.task}</p> <button class="detalles">Detalles</button>
+      <button class="modificar">Modificar</button>
+      <button class="eliminar">Eliminar</button>`
+      nuevaTarea.classList.add("tareas")
+      taskContainer.append(nuevaTarea)
+    })
 
-const taskRescue = document.querySelectorAll(".tareas .eliminar")
-const tareas = taskContainer.querySelectorAll(".tareas")
+  }
 
-taskRescue.forEach((tr,i)=>{
-  tr.addEventListener("click",()=>{
-    tareas[i].remove()
+  const taskRescue = document.querySelectorAll(".tareas .eliminar")
+  const tareas = taskContainer.querySelectorAll(".tareas")
+
+  taskRescue.forEach((tr,i)=>{
+    tr.addEventListener("click",()=>{
+      tareas[i].remove()
+    })
   })
-})
 
-tareas.forEach(()=>{
-  const modalDetalle = document.createElement("div")
-   modalDetalle.innerHTML =  
-  ` <h2>Modificar detalles</h2>
-   <label for="input">Tarea</label>
-   <input type="text" name="tarea">
-   <label for="input">Descripción</label>
-   <input type="text" name="desc">
-   <label for="input">Fecha y Hora entrega</label>
-   <input type="text" name="fecha_hora">
-   <button class="h-25 w-50" id="modificar">Guardar</button>
-`
-modalDetalle.classList.add("modalDetalle")
-modalDetalle.style.display = "none"
-taskContainer.append(modalDetalle)
+  tareas.forEach(()=>{
+    const modalDetalle = document.createElement("div")
+    modalDetalle.innerHTML =  
+    ` <h2>Modificar detalles</h2>
+    <label for="input">Tarea</label>
+    <input type="text" name="tarea">
+    <label for="input">Descripción</label>
+    <input type="text" name="desc">
+    <label for="input">Fecha y Hora entrega</label>
+    <input type="text" name="fecha_hora">
+    <button class="h-25 w-50" id="modificar">Guardar</button>
+  `
+  modalDetalle.classList.add("modalDetalle")
+  modalDetalle.style.display = "none"
+  taskContainer.append(modalDetalle)
 
-})
-const modificar = document.querySelectorAll(".tareas .modificar")
-const modales = document.querySelectorAll(".modalDetalle") 
-
-modificar.forEach((mod, j)=>{
-  mod.addEventListener("click",()=>{
-    modales[j].style.display = "flex"
   })
-})
+  const modificar = document.querySelectorAll(".tareas .modificar")
+  const modales = document.querySelectorAll(".modalDetalle") 
 
-const guardar = document.querySelectorAll("#modificar")
-
-guardar.forEach((gu, k)=>{
-  gu.addEventListener("click",()=>{
-    modales[k].style.display = "none"
+  modificar.forEach((mod, j)=>{
+    mod.addEventListener("click",()=>{
+      modales[j].style.display = "flex"
+    })
   })
-})
+
+  const guardar = document.querySelectorAll("#modificar")
+
+  guardar.forEach((gu, k)=>{
+    gu.addEventListener("click",()=>{
+      modales[k].style.display = "none"
+    })
+  })
 
 })
 
