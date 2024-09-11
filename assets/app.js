22f29edc56bb7f3946eca20c9496691ad8021636
javier lombardero <javier.lombar2@gmail.com>
2024-09-11 21:40:23 +0200

    cambios estilos app

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
-const input = document.querySelector(".input input");
-const addTask = document.querySelector(".input button");
+const input = document.querySelector(".toDo .header input");
+const addTask = document.querySelector(".toDo .header button");
 const taskContainer = document.querySelector(".taskContainer");
 
 let id = 1;
@@ -12,20 +12,31 @@ let crearTarea = (x, y) => {
     alert("solo caracteres alfabéticos");
     return;
   }
+  if(x.value == ""){
+    alert("debes ingresar un valor")
+  }
 
   const newTask = document.createElement("div");
-  newTask.innerHTML = `<div><p>${x}</p> <button class="detalles">Detalles</button>
-<button class="actualizar">Actualizar</button>
-<button class="eliminar" id="${y}">Eliminar</button></div>`;
-  newTask.classList.add("tareas");
+  newTask.innerHTML = ` <div class="taskName">
+                        <p>${x}</p>
+                    </div>
+                    <div class="controlButtons">
+                        <button class="detalle" id="${y}">Details</button>
+
+                        <button class="eliminar"id="${y}">Remove</button>
+                    </div>
+                    <i class="fa-solid fa-font-awesome"></i>
+                    <input type="checkbox" name="" id="${y}">
+`;
+  newTask.classList.add("task");
   taskContainer.append(newTask);
 
-  const task = {
+  const taskObject = {
     id: y,
     nombre: x,
   };
 
-  taskList.push(task);
+  taskList.push(taskObject);
 
   input.value = "";
 
@@ -33,44 +44,50 @@ let crearTarea = (x, y) => {
 
 
     modalDetalle = document.createElement("div");
-    modalDetalle.innerHTML = ` <h2>Modificar detalles</h2>
-  <div class="modalBody">
-   <label for="input">Tarea</label>
-   <input type="text" disabled name="tarea" value="${x}">
-   <button><i class="fa-regular fa-pen-to-square"></i></button>
-   <label for="input">Descripción</label>
-   <input type="text" disabled name="desc">
-     <button><i class="fa-regular fa-pen-to-square"></i></button>
-   <label for="input">Fecha y Hora entrega</label>
-   <input type="date" name="fecha_hora" disabled>
-     <button><i class="fa-regular fa-pen-to-square"></i></button>
-    </div>
-   <button class="h-25 w-50" id="modificar">Guardar</button>`;
-    modalDetalle.classList.add("modalDetalle");
-    modalDetalle.setAttribute("id", y )
+    
+    modalDetalle.innerHTML = `  <button id="closeModal">X</button>
+                <h2>${x}</h2>
+                <div class="modalBody">
+                    <label for="">Nombre  <i class="fa-solid fa-pen-to-square"></i></label>
+                    <input type="text" value=${x} disabled>
+                    <label for="">Descripción  <i class="fa-solid fa-pen-to-square"></i></label>
+                    <input type="text" disabled>
+                    <label for="">Fecha y hora  <i class="fa-solid fa-pen-to-square"></i></label>
+                 <input type="datetime-local" disabled>
+                 <div class="priority">
+                    <p>Priority</p>
+                 <i class="fa-solid fa-font-awesome" style="color:green;"></i>
+                 <i class="fa-solid fa-font-awesome" style="color:rgb(240, 255, 28);"></i>
+                 <i class="fa-solid fa-font-awesome" style="color:rgb(255, 104, 17);"></i>
+                </div>
+
+                    <button id="guardar">Guardar</button> 
+               </div> `;
+    modalDetalle.classList.add("detailsModal");
+    `modalDetalle.setAttribute("id", ${y})` 
     modalDetalle.style.display = "none";
     taskContainer.append(modalDetalle);
   
 
   
   
-  const modificar = document.querySelectorAll(".tareas .detalles");
-  const modales = document.querySelectorAll(".modalDetalle");
+ const modificar = document.querySelectorAll(".task .controlButtons  .detalle");
+  const modales = document.querySelectorAll(".detailsModal");
 
   modificar.forEach((mod, j) => {
     mod.addEventListener("click", () => {
   modales[j].style.display = "flex";
+  inputsModal.disabled = true
     });
   });
 /*
   const posicionModal = modales.filter((pos)=>{
     console.log(pos)
   })
-  const descripcion = mod.querySelectorAll(".modalDetalle .modalBody input[name=desc]")
   
-  const editButton = document.querySelectorAll(
-    ".modalDetalle .modalBody button"
-  );
+  const descripcion = mod.querySelectorAll(".detailsModal .modalBody input[name=desc]")
+  */
+  const editButton = document.querySelectorAll(".detailsModal .modalBody label i");
   const inputsModal = document.querySelectorAll(".modalBody input");
 
   editButton.forEach((edBut, k) => {
@@ -78,19 +95,30 @@ let crearTarea = (x, y) => {
       inputsModal[k].disabled = false;
     });
   });
-  */
+  
 
-  const guardar = document.querySelectorAll(".modalDetalle  #modificar");
+  const cerrar = document.querySelectorAll(".detailsModal  #closeModal");
 
-  guardar.forEach((gu, l) => {
-    gu.addEventListener("click", () => {
-      inputsModal[l].disabled = true;
-      modales[l].style.display = "none";
+  cerrar.forEach((cer,l) => {
+    cer.addEventListener('click', () => {
+       modales[l].style.display = "none"
+      
     });
-  });
+});;
+  const guardar = document.querySelectorAll(".detailsModal  #guardar");
 
-  const delButton = document.querySelectorAll(".tareas  .eliminar");
-  const tareasToDel = taskContainer.querySelectorAll(".tareas");
+  guardar.forEach((gu,q) => {
+    gu.addEventListener('click', () => {
+        modales[q].style.display = "none"
+        inputsModal[q].disabled = true
+    });
+});;
+
+   
+  
+
+  const delButton = document.querySelectorAll(".task  .controlButtons .eliminar");
+  const tareasToDel = taskContainer.querySelectorAll(".task");
 
   delButton.forEach((del, i) => {
     del.addEventListener("click", () => {
@@ -99,14 +127,14 @@ let crearTarea = (x, y) => {
         return taskL.id != del.id;
       });
   
-      taskList = newTaskList
+     taskList = newTaskList
       localStorage.setItem(`tareasJavi`, JSON.stringify(newTaskList));
 
       tareasToDel[i].remove();
     });
   });
-};
-
+}
+  
 addTask.addEventListener("click", () => {
   const nuevaTarea = input.value;
 
