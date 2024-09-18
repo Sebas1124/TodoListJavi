72ad47f57d18ebd59c6a0fafc616a2619f400d6b
javier lombardero <javier.lombar2@gmail.com>
2024-09-18 11:20:21 +0200

    implementer deleteSlelected

--- assets/app.js
+++ assets/app.js
@@ -5,30 +5,34 @@ const taskContainer = document.querySelector(".taskContainer");
 let id = 1;
 let taskList = [];
 
-let crearTarea = (x, y) => {
+let crearTarea = (x, y, z, a, g) => {
   const regex = /^[a-zA-Z0-9\s]*$/;
 
   if (!regex.test(x)) {
     alert("solo caracteres alfabéticos");
     return;
   }
-  if(x.value == ""){
-    alert("debes ingresar un valor")
+  if (x.value == "") {
+    alert("debes ingresar un valor");
+  }
+
+  if (g == true) {
+    return;
   }
 
   const newTask = document.createElement("div");
   newTask.innerHTML = ` <div class="taskName">
                         <p>${x}</p>
-                    </div>
-                    <div class="controlButtons">
+                        </div>
+                        <div class="controlButtons">
                         <button class="detalle" id="${y}">Details</button>
-
                         <button class="eliminar"id="${y}">Remove</button>
-                    </div>
-                    <i class="fa-solid fa-font-awesome"></i>
-                    <input type="checkbox" name="" id="${y}">
-`;
+                        </div>
+                       <i class="fa-solid fa-font-awesome"></i>
+                       <input type="checkbox" name="" id="${y}" ${g}>`;
+
   newTask.classList.add("task");
+  newTask.setAttribute("id", y);
   taskContainer.append(newTask);
 
   const taskObject = {
@@ -40,20 +44,17 @@ let crearTarea = (x, y) => {
 
   input.value = "";
 
- 
+  modalDetalle = document.createElement("div");
 
-
-    modalDetalle = document.createElement("div");
-    
-    modalDetalle.innerHTML = `  <button id="closeModal">X</button>
+  modalDetalle.innerHTML = `<button id="closeModal">X</button>
                 <h2>${x}</h2>
                 <div class="modalBody">
                     <label for="">Nombre  <i class="fa-solid fa-pen-to-square"></i></label>
                     <input type="text" value=${x} disabled>
                     <label for="">Descripción  <i class="fa-solid fa-pen-to-square"></i></label>
-                    <input type="text" disabled>
+                    <input type="text" name="desc" value=${z} disabled>
                     <label for="">Fecha y hora  <i class="fa-solid fa-pen-to-square"></i></label>
-                 <input type="datetime-local" disabled>
+                 <input type="datetime-local" value=${a} disabled>
                  <div class="priority">
                     <p>Priority</p>
                  <i class="fa-solid fa-font-awesome" style="color:green;"></i>
@@ -61,33 +62,25 @@ let crearTarea = (x, y) => {
                  <i class="fa-solid fa-font-awesome" style="color:rgb(255, 104, 17);"></i>
                 </div>
 
-                    <button id="guardar">Guardar</button> 
+                    <button class="guardar" id=${y}>Guardar</button> 
                </div> `;
-    modalDetalle.classList.add("detailsModal");
-    `modalDetalle.setAttribute("id", ${y})` 
-    modalDetalle.style.display = "none";
-    taskContainer.append(modalDetalle);
-  
+  modalDetalle.classList.add("detailsModal");
+  modalDetalle.setAttribute("id", y);
+  modalDetalle.style.display = "none";
+  taskContainer.append(modalDetalle);
 
-  
-  
- const modificar = document.querySelectorAll(".task .controlButtons  .detalle");
+  const modificar = document.querySelectorAll(".task .controlButtons .detalle");
   const modales = document.querySelectorAll(".detailsModal");
 
   modificar.forEach((mod, j) => {
     mod.addEventListener("click", () => {
-  modales[j].style.display = "flex";
-  inputsModal.disabled = true
+      modales[j].style.display = "flex";
+      inputsModal.disabled = true;
     });
   });
-/*
-  const posicionModal = modales.filter((pos)=>{
-    console.log(pos)
-  })
-  
-  const descripcion = mod.querySelectorAll(".detailsModal .modalBody input[name=desc]")
-  */
-  const editButton = document.querySelectorAll(".detailsModal .modalBody label i");
+  const editButton = document.querySelectorAll(
+    ".detailsModal .modalBody label i"
+  );
   const inputsModal = document.querySelectorAll(".modalBody input");
 
   editButton.forEach((edBut, k) => {
@@ -95,52 +88,85 @@ let crearTarea = (x, y) => {
       inputsModal[k].disabled = false;
     });
   });
-  
 
   const cerrar = document.querySelectorAll(".detailsModal  #closeModal");
 
-  cerrar.forEach((cer,l) => {
-    cer.addEventListener('click', () => {
-       modales[l].style.display = "none"
-      
+  cerrar.forEach((cer, l) => {
+    cer.addEventListener("click", () => {
+      modales[l].style.display = "none";
     });
-});;
-  const guardar = document.querySelectorAll(".detailsModal  #guardar");
+  });
+  const guardar = document.querySelectorAll(".detailsModal  .guardar");
 
-  guardar.forEach((gu,q) => {
-    gu.addEventListener('click', () => {
-        modales[q].style.display = "none"
-        inputsModal[q].disabled = true
+  guardar.forEach((gu, q) => {
+    gu.addEventListener("click", () => {
+      const descInput = modales[q].querySelector(".modalBody input[name=desc]");
+      const dateInput = modales[q].querySelector(
+        ".modalBody input[type=datetime-local]"
+      );
+      const desc = descInput.value;
+      const date = dateInput.value;
+      const getTareas = localStorage.getItem("tareasJavi");
+      const getTareasParse = JSON.parse(getTareas);
+
+      getTareasParse.map((tar) => {
+        if (tar.id == gu.id) {
+          tar.descripcion = desc;
+          return (tar.fecha = date);
+        }
+      });
+      localStorage.setItem("tareasJavi", JSON.stringify(getTareasParse));
+      modales[q].style.display = "none";
+      inputsModal[q].disabled = true;
     });
-});;
+  });
 
-   
-  
-
-  const delButton = document.querySelectorAll(".task  .controlButtons .eliminar");
+  const delButton = document.querySelectorAll(
+    ".task  .controlButtons .eliminar"
+  );
   const tareasToDel = taskContainer.querySelectorAll(".task");
 
   delButton.forEach((del, i) => {
     del.addEventListener("click", () => {
-
       const newTaskList = taskList.filter((taskL) => {
         return taskL.id != del.id;
       });
-  
-     taskList = newTaskList
+
+      taskList = newTaskList;
       localStorage.setItem(`tareasJavi`, JSON.stringify(newTaskList));
 
       tareasToDel[i].remove();
     });
   });
-}
-  
+
+  const delSelectedBut = document.querySelector(".generalButtons button");
+  const tasks = document.querySelectorAll(".task");
+  const checkBox = document.querySelectorAll(".task input[type=checkbox]");
+
+  delSelectedBut.addEventListener("click", () => {
+    checkBox.forEach((ch, s) => {
+      if (ch.checked == true) {
+        tasks[s].remove();
+        modales[s].remove();
+      }
+      const bajarTareas = localStorage.getItem("tareasJavi");
+      const bajarTareasParse = JSON.parse(bajarTareas);
+      const convertir = bajarTareasParse.map((tar, n) => {
+        return (tar.checked = checkBox[n].checked);
+      });
+      localStorage.setItem("tareasJavi", JSON.stringify(bajarTareasParse));
+    });
+
+
+    
+  });
+};
+
 addTask.addEventListener("click", () => {
   const nuevaTarea = input.value;
 
   crearTarea(nuevaTarea, id);
   id++;
-
 
   localStorage.setItem(`tareasJavi`, JSON.stringify(taskList));
 });
@@ -150,7 +176,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const tareasPendientes = JSON.parse(getLocalStorage);
 
-  tareasPendientes.forEach((tarPend)=>{
-    crearTarea(tarPend.nombre, tarPend.id)
-  })
+  tareasPendientes.forEach((tarPend) => {
+    crearTarea(
+      tarPend.nombre,
+      tarPend.id,
+      tarPend.descripcion,
+      tarPend.fecha,
+      tarPend.checked
+    );
+  });
 });
